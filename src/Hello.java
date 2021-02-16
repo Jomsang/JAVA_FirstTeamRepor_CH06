d789c51e316907c5e696e55ffb98147850d1d5c7
COM <COM@DESKTOP-TH36TT5>
2021-02-16 12:34:19 +0900

    First-commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String a = "hello java";
+		System.out.println(a);
+		System.out.println(a);
+		System.out.println(a);
+
+	}
+
+}
