126486214475f5978d774763e322a2a4fc3494de
COM <COM@DESKTOP-TH36TT5>
2021-02-16 15:05:23 +0900

    zzzzz

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,17 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("zzz1111");
-<<<<<<< HEAD
 		System.out.println("zzz1111");
 		System.out.println("zzz1111");
-=======
-		System.out.println("zzz1111222222");
-		System.out.println("zzz1111222222333333");
-		System.out.println("zzz1111222222333333");
-		System.out.println("zzz1111222222333333");
->>>>>>> branch 'master' of https://github.com/Jomsang/Java.git
-
+		
 
 	}
-
 }
