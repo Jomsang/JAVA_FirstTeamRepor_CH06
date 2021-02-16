54559bbb7af9fa0716a85dedb5a52d9146f39f05
COM <COM@DESKTOP-TH36TT5>
2021-02-16 15:15:17 +0900

    소스 수정

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,12 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("zzz1111");
-
-		System.out.println("zzz1111222222");
-		System.out.println("zzz1111222222333333");
-		System.out.println("zzz1111222222333333");
-		System.out.println("zzz1111222222333333");
-
+		System.out.println("zzz1111");
+		System.out.println("zzz1111");
+		System.out.println("zzz123411");
+		
 
 	}
 }
